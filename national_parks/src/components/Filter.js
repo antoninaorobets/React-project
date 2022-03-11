5fccd83722b0312bd0d395e853813bd7b26a665d
antoninaorobets <pani.antonina.don@gmail.com>
2022-03-11 13:26:36 -0500

    Implement filter functionality

--- national_parks/src/components/Filter.js
+++ national_parks/src/components/Filter.js
@@ -1,31 +1,40 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import states from "../states.json"
 import { Menu } from 'semantic-ui-react'
 
-export default class MenuExampleVerticalSecondary extends Component {
-  state = { activeItem: 'home' }
+function Filter({filterBy, setFilterBy}) {
+   
+  const handleItemClick = (e) => {
+    if (e.target.text !== 'All States') {
+      setFilterBy(states.find(state => state.name === e.target.text).abbreviation)}
+      else {
+        setFilterBy('All States')
+      }
+  }
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
-
-  
-  render() {
-    const { activeItem } = this.state
-    const statesList =   states.map(st=><Menu.Item
+    const statesList = states.map(st=>{
+      const selected = st.abbreviation
+      return (
+      <Menu.Item
         name={st.name}
-        active={activeItem === ""}
-        onClick={this.handleItemClick}
-      /> )
-
+        active = {filterBy === selected}
+        onClick={handleItemClick}
+      />
+      )} 
+      )
 
     return (
       <Menu pointing secondary vertical>
         <Menu.Item
           name='All States'
-          active={activeItem === 'All States'}
-          onClick={this.handleItemClick}
+          active = {filterBy === 'All States'}
+          onClick={handleItemClick}
         />
         {statesList}
       </Menu>
     )
   }
-}
+
+
+export default Filter;
+
--- national_parks/src/components/ParksList.js
+++ national_parks/src/components/ParksList.js
@@ -1,4 +1,4 @@
-import React, { useState }  from 'react'
+import React, { useEffect, useState }  from 'react'
 import parks_data from "../db_test.json"
 import ParkCard from "./ParkCard"
 import Filter from "./Filter"
@@ -13,28 +13,47 @@ import {
 
 
 function ParksList() {
-    const [parks, setParks] = useState(parks_data.parks[0].data)
- 
-    const display_parks_Cards = parks.map(park => <ParkCard key={park.id} park={park}/>)
+    const allParks = parks_data.parks[0].data
+    const [parks, setParks] = useState(allParks)
+    const [filterBy, setFilterBy] = useState('All States')
+
+
+useEffect (()=>{
+  if (filterBy !== 'All States') {
+    const displayParks = parks.filter(park => park.states === filterBy)
+    setParks(displayParks)
+  }
+  else {
+    setParks(allParks)
+  }
+}, [filterBy])
+   
+
+    //const displayParks = parks.filter(park => park.states === filterBy)
+
+    console.log(parks)
+    const parksCards = parks.map(park => <ParkCard key={park.id} park={park}/>)
+    
+
   return (
     <div>
       <Header as='h1' style={{ marginTop: '5em', marginBottom: '1em' }}>National Parks in the United States</Header>
       <Grid relaxed>
         <Grid.Row>
           <Grid.Column width={4}>
-            <Filter />
+            <Filter filterBy={filterBy} setFilterBy={setFilterBy} />
           </Grid.Column>
 
           <Grid.Column width={12}>
           <Card.Group  >
-          {display_parks_Cards}
+          {parksCards}
         </Card.Group>
           </Grid.Column>
         </Grid.Row>
       </Grid>
 
       <Divider inverted section />
-      <ParkInformation park={parks[1]} />
+      {/* <ParkInformation park={parks[1]} /> */}
     </div>
   )
 }
