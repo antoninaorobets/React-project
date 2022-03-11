8d3f8173552734b80bfd1de5ce962f5b34903d98
antoninaorobets <pani.antonina.don@gmail.com>
2022-03-11 15:34:42 -0500

    db parks full list

--- national_parks/src/components/App.js
+++ national_parks/src/components/App.js
@@ -3,14 +3,22 @@ import '../css/App.css';
 import ParksList from "./ParksList"
 import TopMenu from "./TopMenu"
 import BottomMenu from "./Bottommenu"
+import {  Search } from 'semantic-ui-react'
+import useParksQuery from '../hooks/useParksQuery'
 
 
 function App() {
-  
+
+
 
 return (
   <div className="App">
-    <TopMenu></TopMenu>
+    <TopMenu>
+        <Search 
+          style={{ 'align-items': 'center', 'display': 'flex'  }}
+          fluid
+            />
+    </TopMenu>
     <ParksList />
     <BottomMenu />
   </div>
